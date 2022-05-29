89717a18cce8f2bd5e41012662edda4fe63da5a0
Tad-z <96595227+Tad-z@users.noreply.github.com>
2022-05-29 09:44:06 +0100

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'David Adarabiyo',
+    username: 'Dababez',
+    email: 'Daslut@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Edochie Nnamdi',
+    username: 'OTU',
+    email: 'Otu@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Ayodeji Iseoluwa',
+    username: 'AYO',
+    email: 'Ayo@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Iruene siki',
+    username: 'Siki',
+    email: 'Siki@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Adeniyi Ibukun',
+    username: 'Niyi',
+    email: 'Niyi@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Okuneye David',
+    username: 'Contacts',
+    email: 'contacts@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Deree',
+    username: 'Uncle_dere',
+    email: 'Dere@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Ozuzu Ebube',
+    username: 'Ozu_bu',
+    email: 'ozuzu@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Alex Ipenko',
+    username: 'Alexander',
+    email: 'xander@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Maxwell',
+    username: 'Maxwell',
+    email: 'maxx@gmail.com'
+  }
+];
